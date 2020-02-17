1d8ff7332291af8357ac6f28edf9a9fb699b215c
Hunter <hmvitous@yahoo.com>
2020-02-17 12:38:55 +0100

    Makes a very basic hello world page using react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="main-div">
+    <div>
+    <h1>Hello World</h1>
+    <div>This is the hello world with React</div>
+    </div>
+
+    </div>
+  )
+}
+
+export default App
